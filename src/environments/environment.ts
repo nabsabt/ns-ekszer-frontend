76d62718dc0ed96fc00da06c0fafa507588a8ced
nabsabt <nabsabt@gmail.com>
2025-02-28 13:37:54 +0100

    navbar basics done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   //apiURL: 'http://localhost:3000/api',
   apiURL: 'http://80.99.13.17:3000/api',
   catalogPath: '',
-  isUnderDevelopment: true,
+  isUnderDevelopment: false,
 };
