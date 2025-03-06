002f3091d708638c55c41a482e366469ef41b64a
nabsabt <nabsabt@gmail.com>
2025-03-06 15:05:53 +0100

    beta version finished

--- src/app/@Component/home.component/home.component.ts
+++ src/app/@Component/home.component/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { environment } from '../../../environments/environment';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'home-component',
@@ -8,7 +9,18 @@ import { environment } from '../../../environments/environment';
   styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  constructor(private activatedRoute: ActivatedRoute) {}
+  private isMobileView: boolean;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private breakpointObserver: BreakpointObserver
+  ) {
+    this.breakpointObserver
+      .observe('(max-width: 765px)')
+      .subscribe((result) => {
+        this.isMobileView = result.matches;
+      });
+  }
 
   ngOnInit() {
     this.activatedRoute.fragment.subscribe((fragment: string | null) => {
@@ -17,8 +29,18 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   jumpToSection(section: string | null) {
-    if (section) {
-      document.getElementById(section)?.scrollIntoView({ behavior: 'smooth' });
+    /**Desktop view */
+    if (section && !this.isMobileView) {
+      document.getElementById(section)?.scrollIntoView({
+        behavior: 'smooth',
+        block: section !== 'jewelryMaking' ? 'center' : 'start',
+      });
+    } else if (section && this.isMobileView) {
+      /**Mobile view */
+      document.getElementById(section)?.scrollIntoView({
+        behavior: 'smooth',
+        block: section !== 'goldAndJewelAcquisition' ? 'start' : 'center',
+      });
     }
   }
 
